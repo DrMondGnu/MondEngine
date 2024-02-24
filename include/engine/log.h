548872e59a628368080583dab827c580813cb588
MondGnu <mondsuppe24@gmail.com>
2024-02-24 11:35:22 +0100

    fixed mondengine cmake including and other stuff

--- include/engine/log.h
+++ include/engine/log.h
@@ -15,15 +15,15 @@ namespace mondengine {
 
     class Log {
     public:
-        static void init();
+        MOND_API static void init();
 
-        MOND_API inline static std::shared_ptr<spdlog::logger> get_mond_engine_logger() {return mp_mond_engine_logger;}
-        MOND_API inline static std::shared_ptr<spdlog::logger> get_app_logger () {return mp_app_logger;}
+        MOND_API static const std::shared_ptr<spdlog::logger> &get_mond_engine_logger();
+
+        MOND_API static const std::shared_ptr<spdlog::logger> &get_app_logger();
 
     private:
         static std::shared_ptr<spdlog::logger> mp_mond_engine_logger;
         static std::shared_ptr<spdlog::logger> mp_app_logger;
-
     };
 
 } // mondengine
@@ -34,11 +34,11 @@ namespace mondengine {
 #define MOE_WARN(...) mondengine::Log::get_mond_engine_logger()->warn(__VA_ARGS__)
 #define MOE_ERROR(...) mondengine::Log::get_mond_engine_logger()->error(__VA_ARGS__)
 
-#define APP_TRACE(...) mondengine::Log::get_mond_engine_logger()->trace(__VA_ARGS__)
-#define APP_DEBUG(...) mondengine::Log::get_mond_engine_logger()->debug(__VA_ARGS__)
-#define APP_INFO(...) mondengine::Log::get_mond_engine_logger()->info(__VA_ARGS__)
-#define APP_WARN(...) mondengine::Log::get_mond_engine_logger()->warn(__VA_ARGS__)
-#define APP_ERROR(...) mondengine::Log::get_mond_engine_logger()->error(__VA_ARGS__)
+#define APP_TRACE(...) mondengine::Log::get_app_logger()->trace(__VA_ARGS__)
+#define APP_DEBUG(...) mondengine::Log::get_app_logger()->debug(__VA_ARGS__)
+#define APP_INFO(...) mondengine::Log::get_app_logger()->info(__VA_ARGS__)
+#define APP_WARN(...) mondengine::Log::get_app_logger()->warn(__VA_ARGS__)
+#define APP_ERROR(...) mondengine::Log::get_app_logger()->error(__VA_ARGS__)
 
 
 #endif //MONDENGINE_LOG_H
--- src/engine/log.cpp
+++ src/engine/log.cpp
@@ -10,8 +10,19 @@ namespace mondengine {
 
     void Log::init() {
         auto stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        stdout_sink->set_pattern("%n-%r-%l: %v");
+        stdout_sink->set_pattern("%n-%T-%l: %v");
         mp_mond_engine_logger = std::make_shared<spdlog::logger>("MondEngine", stdout_sink);
         mp_app_logger = std::make_shared<spdlog::logger>("App", stdout_sink);
     }
+
+    const std::shared_ptr<spdlog::logger> &Log::get_mond_engine_logger()
+    {
+        return mp_mond_engine_logger;
+    }
+
+    const std::shared_ptr<spdlog::logger> &Log::get_app_logger()
+    {
+        return mp_app_logger;
+    }
+
 } // mondengine
